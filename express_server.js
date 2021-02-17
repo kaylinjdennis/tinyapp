2dc8f483aacb1c417fb63ef0422d61f1bc00e999
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-17 00:07:43 -0700

    Fixed delete buttons and push request for delete

--- express_server.js
+++ express_server.js
@@ -28,10 +28,7 @@ app.post('/urls', (req, res) => {
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
-  // console.log(`about to delete ${req.params.shortURL}`);
   delete urlDatabase[req.params.shortURL];
-  // console.log(`done deleting`);
-  // console.log(urlDatabase);
   res.redirect('/urls');
 });
 
